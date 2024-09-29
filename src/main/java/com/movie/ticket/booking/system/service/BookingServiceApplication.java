ca97602818444eb6ac4c4878d96e187e6ca4361c
Unknown <unikdahal@archlinux.localdomain>
2024-09-29 20:12:44 +0530

    first commit

---
+++ src/main/java/com/movie/ticket/booking/system/service/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.ticket.booking.system.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/ticket/booking/system/service/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.ticket.booking.system.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
